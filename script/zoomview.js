86fae2ea89aecbdda4f9207e858daf506910e1a2
Hatmic <rongxuan.zhang@outlook.com>
2024-04-27 10:12:46 +0800

    将错误的命名更改为 ZoomView（放大阅览效果）

---
+++ script/zoomview.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let zoomViewButton = document.querySelector(".zoomview");
+
+    let currentUrl = window.location.href
+    if (currentUrl.includes("?zoomview")) {
+        addZoomView();
+    }
+
+    zoomViewButton.addEventListener("click", function() {
+        if (currentUrl.includes("?zoomview")) {
+            currentUrl = currentUrl.replace("?zoomview","");
+            removeZoomView();
+        } else {
+            currentUrl += "?zoomview";
+            addZoomView();
+        }
+
+        window.location.href = currentUrl;
+    });
+
+    function addZoomView() {
+        let linkElement = document.createElement("link");
+        linkElement.setAttribute("rel", "stylesheet");
+        linkElement.setAttribute("id", "zoomview");
+        linkElement.setAttribute("href", "./style/zoomview.css");
+        document.head.appendChild(linkElement);
+    }
+
+    function removeZoomView() {
+        let zoomviewLink = document.getElementById("zoomview");
+        if (zoomviewLink) {
+            zoomviewLink.remove();
+        }
+    }
+});
