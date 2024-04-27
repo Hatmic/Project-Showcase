b4b7703b2daf03284231ebfed60a0edefe148311
Hatmic <rongxuan.zhang@outlook.com>
2024-04-27 20:19:11 +0800

    Organization 模板

--- script/index.js
+++ script/index.js
@@ -48,3 +48,69 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.querySelector('#footerAvatar').style.height = navOptionHeight * 1 + 'px';
 });
+
+let container0 = document.getElementById("nav");
+navOptionsInformation.forEach(function(i){
+    let navOptionInformationHtml = `
+        <a href="${i.link}" class="option text-with-underline" id="${i.id}"><p>${i.opName}</p></a>
+    `;
+    container0.innerHTML += navOptionInformationHtml;
+});
+
+let container2 = document.getElementById("intro-1");
+intro1Information.forEach(function(i){
+    let intro1InformationHtml = `
+        <p>${i.para}</p>
+    `;
+    container2.innerHTML += intro1InformationHtml;
+});
+
+let container3 = document.getElementById("intro-2");
+intro2Information.forEach(function(i){
+    let intro2InformationHtml = `
+        <p>${i.para}</p>
+    `;
+    container3.innerHTML += intro2InformationHtml;
+});
+
+let container1 = document.getElementById("repositories");
+repositoriesInformation.forEach(function(i, index1){
+    let repositoriesInformationHtml = `
+        <a href="${i.website}" target="_blank"><h3>${i.projectName}</h3></a>
+        <p class="intro">${i.intro}</p>
+        <p class="intro">GitHub: <a href="${i.githubUrl}" target="_blank">${i.githubName}</a></p>
+        <p class="intro">Author: <a href="${i.author_link}" target="_blank">${i.authour}</a></p>
+    `;
+    if(index1 !== repositoriesInformation.length - 1){
+        repositoriesInformationHtml += `<hr>`
+    }
+    container1.innerHTML += repositoriesInformationHtml;
+});
+
+let container4 = document.getElementById("organization_");
+orgInformation.forEach(function(i){
+    let orgInformationHtml = `
+        <div class="hatmic-org orgCard">
+            <span class="orgAvatarSpan"><a href="${i.orgLink}" target="_blank"><img src="${i.orgAvatarLink}" alt="" class="orgAvatar"></a></span>
+            <span class="orgProfileSpan">
+                <div>
+                    <h3 class="orgNickname">${i.orgNickname}</h3>
+                    <h3 class="orgId">${i.orgId}</h3>
+                </div>
+                <br>
+                <div class="member">
+                    
+                </div>
+            </span>
+        </div>
+    `;
+    container4.innerHTML += orgInformationHtml;
+});
+
+let container5 = document.querySelector(".member");
+orgMember.forEach(function(i){
+    let orgMemberHtml = `
+        <img src="${i.imgLink}" alt="">
+    `;
+    container5.innerHTML += orgMemberHtml;
+});
--- script/information.js
+++ script/information.js
@@ -1,31 +1,38 @@
+// Here is script/information.js, where you can change the text, images, and other content you want to display.
+// Visit this link to browse the GitHub repository of the original author, Hatmic: https://github.com/Hatmic/Project-pro.hatmic.com
+// Copyright © 2024 Hatmic
+
+// Nav
 let navOptionsInformation = [
     {
         link: "",
         opName: "Theme",
-        id: "themeButton"
+        id: "themeButton" // Hatmic: I Wouldn't recommend deleting this ID.
     },
     {
-        link: "#",
+        link: "#home",
         opName: "Home",
-        id: "option"
+        id: "option" // Hatmic: I Wouldn't recommend deleting this ID.
     },
     {
-        link: "#",
+        link: "#about",
         opName: "About",
         id: ""
     },
     {
-        link: "#",
+        link: "#blog",
         opName: "Blog",
         id: ""
     },
     {
-        link: "#",
+        link: "#music",
         opName: "Music",
         id: ""
     }
 ];
+//
 
+// Introduction
 let intro1Information = [
     {
         para: "I am a middle school student from China who is interested in both music and computer science fields. You can call me Hatmic."
@@ -43,7 +50,36 @@ let intro2Information = [
         para: "Feel free to follow me on GitHub at this link: <a href=\"https://github.com/Hatmic\">https://github.com/Hatmic</a>!"
     }
 ];
+//
 
+// Organization
+// If you haven't made corresponding modifications in script/index.js, it's not recommended to fill in content from multiple repositories.
+let orgInformation = [
+    {
+        orgLink: "https://github.com/Hatmic-Org/",
+        orgAvatarLink: "https://avatars.githubusercontent.com/u/167613474?s=400&u=86366aea106dc688d420345b29b692a755662125&v=4",
+        orgNickname: "Hatmic Organization",
+        orgId: "hatmic-org",
+    }
+];
+
+let orgMember = [
+    {
+        imgLink: "https://avatars.githubusercontent.com/u/103878671?v=4",
+    },
+    {
+        imgLink: "https://avatars.githubusercontent.com/u/111544765?v=4",
+    },
+    {
+        imgLink: "https://avatars.githubusercontent.com/u/132246733?v=4",
+    },
+    {
+        imgLink: "https://avatars.githubusercontent.com/u/150152150?v=4"
+    }
+];
+//
+
+// Star Repositories
 let repositoriesInformation = [
     {
         projectName: "pro.hatmic.com",
@@ -91,41 +127,4 @@ let repositoriesInformation = [
         author_link: "https://github.com/Hatmic"
     }
 ];
-
-let container0 = document.getElementById("nav");
-navOptionsInformation.forEach(function(i){
-    let navOptionInformationHtml = `
-        <a href="${i.link}" class="option text-with-underline" id="${i.id}"><p>${i.opName}</p></a>
-    `;
-    container0.innerHTML += navOptionInformationHtml;
-});
-
-let container2 = document.getElementById("intro-1");
-intro1Information.forEach(function(i){
-    let intro1InformationHtml = `
-        <p>${i.para}</p>
-    `;
-    container2.innerHTML += intro1InformationHtml;
-});
-
-let container3 = document.getElementById("intro-2");
-intro2Information.forEach(function(i){
-    let intro2InformationHtml = `
-        <p>${i.para}</p>
-    `;
-    container3.innerHTML += intro2InformationHtml;
-});
-
-let container1 = document.getElementById("repositories");
-repositoriesInformation.forEach(function(i, index1){
-    let repositoriesInformationHtml = `
-        <a href="${i.website}" target="_blank"><h3>${i.projectName}</h3></a>
-        <p class="intro">${i.intro}</p>
-        <p class="intro">GitHub: <a href="${i.githubUrl}" target="_blank">${i.githubName}</a></p>
-        <p class="intro">Author: <a href="${i.author_link}" target="_blank">${i.authour}</a></p>
-    `;
-    if(index1 !== repositoriesInformation.length - 1){
-        repositoriesInformationHtml += `<hr>`
-    }
-    container1.innerHTML += repositoriesInformationHtml;
-});
+//
