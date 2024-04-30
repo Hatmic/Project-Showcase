b8fa490a0a05b0c74264cd834f8e42ca50f864d9
Hatmic <rongxuan.zhang@outlook.com>
2024-04-30 23:12:46 +0800

    CSS => SCSS

--- script/theme.js
+++ script/theme.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
         let linkElement = document.createElement("link");
         linkElement.setAttribute("rel", "stylesheet");
         linkElement.setAttribute("id", "darkStylesheet");
-        linkElement.setAttribute("href", "./style/dark.css");
+        linkElement.setAttribute("href", "/css/dark.css");
         document.head.appendChild(linkElement);
     }
 
